5d36528dd7e72b6ba78e9e668850b1962eb36e5a
moksh555 <57554299+moksh555@users.noreply.github.com>
2025-12-28 21:40:13 -0600

    daily problem: 12/29/2025

---
+++ Daily_Problems/756_Pyramid_Transition_Matrix.py
@@ -0,0 +1,31 @@
+
+from collections import defaultdict
+from functools import cache
+
+
+class Solution:
+    def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
+        # seeting up lookup table, what this lookup table, what this will do is directly helps us to traverse the top lement for left+right without iterating whole allowed array everytime, a little optimization 
+        lookup = defaultdict(list) #lookup = {ab: [c,e,w], eb:[t,y,u]} so here "a" and "b" together "ab", are bottom left and bottom right block for which there are [c,e,w] block that can go on top
+
+        for l,r,t in allowed:
+            lookup[l+r].append(t) #each elemnt of allowed is three letters example "abc" so here l=a, r=b, t=c and giving and sotring lookup[ab].append(c)
+        
+        # main helper function to run backtracing and @cache helps to store similar result in memory so it can be reused again if same exists, so in another sense we are using memoization to resuse already visited path
+        @cache
+        def helper(ind, start, new):# new -> the string which we can build from start and lookuptable, start is the already build layers or bottom layer to which we are building "new" or top layer, and "ind" is again the index of which new layer we are on
+            
+            if len(start) == 1:# if length of start == 1, we will return Truw which means we ahve reached the topmost layer and no further layer possible
+                return True 
+            
+            if ind == len(start)-1: # so if "new" string is built full which we can say from if "ind" is at len(start)-1 index which means the layer is already and we should go and build top layer which now start = new which will be bottom layer and new will be empty string as we will start to build new string absed on the bottom layer
+                return helper(0, new, "")
+            
+            # getting bottom left and right to build new layer from bottom or start layer
+            l = start[ind]
+            r = start[ind+1]
+            for char in lookup[l+r]:# iterating thorugh every possible answer for left and right combination in lookup table
+                if helper(ind+1, start, new + char): # if any of it returns True we directly return True 
+                    return True
+            return False # if none returns True this will return False
+        return helper(0,bottom, "") # calling helper fucntion with ind = 0 as will start to build the top layer for bottom and new will be empty string
